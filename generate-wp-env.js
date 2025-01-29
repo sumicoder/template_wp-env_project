ef5e21b153cfc2e5e14165e7f670b7a3ed6283ac
sumi <121598930+sumicoder@users.noreply.github.com>
2025-01-29 09:21:14 +0900

    ポート番号追加

--- generate-wp-env.js
+++ generate-wp-env.js
@@ -2,21 +2,26 @@ const fs = require('fs');
 require('dotenv').config();
 
 const wpEnvConfig = {
-    core: "https://ja.wordpress.org/latest-ja.zip",
+    core: 'https://ja.wordpress.org/latest-ja.zip',
     phpVersion: null,
     plugins: [],
     themes: [],
     config: {
-        WP_DEBUG: true
+        WP_DEBUG: true,
+    },
+    port: 12345,
+    env: {
+        tests: {
+            port: 12346,
+        },
     },
     mappings: {
         [`wp-content/themes/${process.env.WP_THEME_NAME}/`]: `./dist/${process.env.WP_THEME_NAME}/`,
-        "wp-content/plugins/": "./dist/plugins/",
-        "wp-content/uploads/": "./dist/uploads/",
-        "sql": "./sql/",
-        ".htaccess": ".htaccess"
-    }
+        'wp-content/plugins/': './dist/plugins/',
+        'wp-content/uploads/': './dist/uploads/',
+        sql: './sql/',
+        '.htaccess': '.htaccess',
+    },
 };
 
 fs.writeFileSync('.wp-env.json', JSON.stringify(wpEnvConfig, null, 4));
-
